b670cf9fdf70fac95ce4fbbdd8787267760eff9e
Vince Cima <contact@vincecima.com>
2016-01-30 11:40:35 -0600

    Remove execution of wireless drivers, that has to be done before anything else

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -3,5 +3,4 @@ msiexec /i just-install.msi
 just-install update
 just-install 1password 7zip atom crashplan dropbox github google-chrome handbrake lockhunter mpc-hc plex-home-theater plex-media-server qbittorrent sharpkeys slack sumatrapdf vagrant windirstat ynab
 
-.\A6210_v1_0_0_34.exe
 .\WinSplit-Revolution-v11.04.exe /S
