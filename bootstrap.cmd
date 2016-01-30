bf7cf813dfc2104ff52124de17500297181f51ab
Vince Cima <contact@vincecima.com>
2016-01-30 11:14:40 -0600

    Add plex server to packages

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,7 +1,7 @@
 msiexec /i just-install.msi
 
 just-install update
-just-install 1password 7zip atom crashplan dropbox github google-chrome handbrake lockhunter mpc-hc plex-home-theater qBittorrent sharpkeys slack sumatrapdf vagrant windirstat ynab
+just-install 1password 7zip atom crashplan dropbox github google-chrome handbrake lockhunter mpc-hc plex-home-theater plex-server qBittorrent sharpkeys slack sumatrapdf vagrant windirstat ynab
 
 .\A6210_v1_0_0_34.exe
 .\WinSplit-Revolution-v11.04.exe /S
