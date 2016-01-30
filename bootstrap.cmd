1ed1b8b80863265c5d07bb3a541a68ce699cb73f
Vince Cima <contact@vincecima.com>
2016-01-30 10:46:53 -0600

    Start of bootstrap using just-install

---
+++ bootstrap.cmd
@@ -0,0 +1,6 @@
+msiexec /i just-install.msi
+
+just-install update
+just-install 1password 7zip atom crashplan dropbox github google-chrome handbrake lockhunter mpc-hc sharpkeys slack sumatrapdf vagrant windirstat
+
+.\A6210_v1_0_0_34.exe
