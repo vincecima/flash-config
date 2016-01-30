623aa9a24eb982a9059624cb29880255a76766a2
Vince Cima <contact@vincecima.com>
2016-01-30 10:53:23 -0600

    Add WinSplit Revolution (no longer hosted)

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -4,3 +4,4 @@ just-install update
 just-install 1password 7zip atom crashplan dropbox github google-chrome handbrake lockhunter mpc-hc sharpkeys slack sumatrapdf vagrant windirstat
 
 .\A6210_v1_0_0_34.exe
+.\WinSplit-Revolution-v11.04.exe /S
